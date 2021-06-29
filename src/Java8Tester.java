946466f0b3343baa4be56a12383e816aa59d418a
furude.K <hurudestars@gmail.com>
2021-06-29 21:00:28 +0900

    Done RamdaPractice

--- src/Java8Tester.java
+++ src/Java8Tester.java
@@ -4,7 +4,8 @@ import java.util.stream.Stream;
 public class Java8Tester {
     public static void main(String[] args) {
         //ListPractice.listPractice(); //Listの練習用プログラム
-        StreamAPIpractice.streamAPIpractice(); //StreamAPI練習用のプログラム
+        //StreamAPIpractice.streamAPIpractice(); //StreamAPI練習用のプログラム
+        RamdaPractice.ramda(); //ラムダ練習用プログラム
     }
 
 }
--- src/RamdaPractice.java
+++ src/RamdaPractice.java
@@ -1,13 +1,66 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
+
 public class RamdaPractice {
+    static void print(Integer i) {
+        System.out.println(i);
+    }
+
     static void ramda() {
+        /** メソッド参照を紐解く*/
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5); //1~5までの要素を持つリストを作成
+        //メソッド参照(System.out::println）
+        /** 今回はよりわかりやすくするために、printメソッドを用意*/
+        //クラス名::メソッド名でメソッド参照が可能
+        list.stream().forEach(RamdaPractice::print);
 
-        //メソッド参照を紐解く
-        list.stream().forEach(System.out::println);
+        // ラムダ式で記入するとこうなる
+        list.stream().forEach(i -> print(i)); //引数iをprintメソッドに渡して表示をする
 
+        //匿名クラスでの実装
+        Consumer<Integer> hoge = new Consumer<>(){
+            @Override
+            public void accept(Integer  i) {
+                System.out.println(i);
+            }
+        };
+        Stream<Integer> list2 =list.stream();
+        list2.forEach(hoge);
+
+        //for文で書く
+        for(Integer i : list){
+            System.out.println(i);
+        }
+
+        /** ラムダ式はローカルクラスと無名クラスという仕組みで成り立っている */
+
+        //Runnableインタフェースを実装したローカルクラス
+        class Local implements Runnable {
+            @Override
+            public void run() {
+                System.out.println("Hello Lambda!");
+            }
+        }
+
+        Runnable person = new Local();
+        person.run(); // Hello Lambda!
+
+        //無名クラス = インタフェースを実装したローカルクラスの宣言を省略（26行目）
+        Runnable mumeiPerson = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello Lambda!");
+            }
+        };
+        mumeiPerson.run(); //Hello Lambda!
+
+        //ラムダ式 無名クラスからさらにnew Runnable()とpublic void runを省略
+        Runnable ramdaPerson = () -> { System.out.println("Hello Lambda!"); };
+        ramdaPerson.run(); //Hello Lambda
+
+        //抽象メソッドが1つでなければいけない
     }
 }
